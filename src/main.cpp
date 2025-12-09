52e1914072261dad7be983eba162a0457af0b45a
Taiga Saito <me@taigasaito.org>
2025-12-10 07:05:34 +0900

    feat: subscriber を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,84 @@
 #include <Arduino.h>
 #include <micro_ros_platformio.h>
+#include <rcl/rcl.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <std_msgs/msg/int32.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only available for Arduino framework with serial transport.
+#endif
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define LED_PIN 13
+
+#define RCCHECK(fn)                \
+  {                                \
+    rcl_ret_t temp_rc = fn;        \
+    if ((temp_rc != RCL_RET_OK)) { \
+      error_loop();                \
+    }                              \
+  }
+#define RCSOFTCHECK(fn)            \
+  {                                \
+    rcl_ret_t temp_rc = fn;        \
+    if ((temp_rc != RCL_RET_OK)) { \
+    }                              \
+  }
+
+// Error handle loop
+void error_loop() {
+  while (1) {
+    Serial.println("Error occurred! Halting execution.");
+    delay(100);
+  }
+}
+
+void subscription_callback(const void* msgin) {
+  const std_msgs__msg__Int32* msg = (const std_msgs__msg__Int32*)msgin;
+  digitalWrite(LED_PIN, HIGH);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Configure serial transport
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  delay(2000);
+
+  digitalWrite(LED_PIN, LOW);
+
+  allocator = rcl_get_default_allocator();
+
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "arduino_sub_test", "", &support));
+
+  // create subscription
+  RCCHECK(rclc_subscription_init_default(
+      &subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+      "arduino_sub_test_subscription"));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg,
+                                         subscription_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
-
-// put function definitions here:
-int myFunction(int x, int y) { return x + y; }
